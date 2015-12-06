b773e2fc019d7df8aa20444379551b2a2c9046c0
emonmr <emonmr@gmail.com>
2015-12-06 19:05:36 +0600

    try to install phpdebugbar

--- bootstrap.php
+++ bootstrap.php
@@ -2,23 +2,24 @@
 use Propel\Common\Config\ConfigurationManager;
 use Propel\Runtime\Connection\ConnectionManagerSingle;
 use Propel\Runtime\Propel;
+use DebugBar\DebugBar;
 
-define('APP_DIR', __DIR__.DIRECTORY_SEPARATOR);
+define('APP_DIR', __DIR__ . DIRECTORY_SEPARATOR);
 
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
 date_default_timezone_set('Asia/Dhaka');
 session_start();
 
-require APP_DIR.'vendor/autoload.php';
+require APP_DIR . 'vendor/autoload.php';
 //Propel::init('../propel/propel.xml');
 
 // Load the configuration file
-$configManager = new ConfigurationManager( '../propel/propel.xml' );
+$configManager = new ConfigurationManager('../propel/propel.xml');
 
 // Set up the connection manager
 $manager = new ConnectionManagerSingle();
-$manager->setConfiguration( $configManager->getConnectionParametersArray()[ 'default' ] );
+$manager->setConfiguration($configManager->getConnectionParametersArray()['default']);
 $manager->setName('default');
 
 // Add the connection manager to the service container
@@ -31,23 +32,40 @@ $serviceContainer->setDefaultDatasource('default');
 
 // Prepare app
 $app = new \Slim\Slim(array(
-    'templates.path' => APP_DIR.'templates',
+    'templates.path' => APP_DIR . 'templates',
 ));
-//$debugbar->addCollector(new DebugBar\Bridge\SlimCollector($app));
 
 // Create monolog logger and store logger in container as singleton
 // (Singleton resources retrieve the same log resource definition each time)
-$app->container->singleton('log', function () {
+/*$app->container->singleton('log', function () {
     $log = new \Monolog\Logger('slim-skeleton');
-    $log->pushHandler(new \Monolog\Handler\StreamHandler(APP_DIR.'tmp/app.log', \Monolog\Logger::DEBUG));
+    $log->pushHandler(new \Monolog\Handler\StreamHandler(APP_DIR . 'tmp/app.log', \Monolog\Logger::DEBUG));
     return $log;
-});
+});*/
 
+/*$app->hook('slim.before', function () use ($app) {
+
+
+});*/
+
+//set global variable from here
+$app->hook('slim.before.dispatch', function () use ($app) {
+
+    $debugbar = new DebugBar();
+
+    $debugbar->addCollector(new \DebugBar\Bridge\SlimCollector($app));
+
+    $debugbarRenderer = $debugbar->getJavascriptRenderer();
+
+    $app->view()->setData(array('debugbarhead' => $debugbarRenderer->renderHead(),
+        'debugbarbody' => $debugbarRenderer->render()));
+
+});
 // Prepare view
 $app->view(new \Slim\Views\Twig());
 $app->view->parserOptions = array(
     'charset' => 'utf-8',
-    'cache' => realpath(APP_DIR.'tmp/cache'),
+    'cache' => realpath(APP_DIR . 'tmp/cache'),
     'auto_reload' => true,
     'strict_variables' => false,
     'autoescape' => true
